f1d65d88fe2aafb900972be0517304be65cfa1f4
matiaslallave <mallavequez@outlook.com>
2021-08-09 14:26:44 +0200

    a better logic to search filter

--- src/components/task-item/index.jsx
+++ src/components/task-item/index.jsx
@@ -2,50 +2,38 @@ import "./style.css";
 import AdjustIcon from "@material-ui/icons/Adjust";
 import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
 import React from "react";
+import { useState, useEffect } from "react";
 
 function TaskItem(props) {
+
+  const [isHidden, setIsHidden] = useState('');
 
   const handleDelete = () => {
     props.deleteCard(props.task.id);
   };
 
+  useEffect (() => {
+    props.task.title.toLowerCase().includes(props.searchFilter) ? setIsHidden('') : setIsHidden('display-none');
+  }, [props.searchFilter, props.task.title]);
+
   return (
     <React.Fragment>
-      {props.task.title.toLowerCase().includes(props.searchFilter) ? (
-        <li className={`list-element`}>
-          {props.task.done ? (
-            <CheckCircleOutlineIcon className="icon-done"></CheckCircleOutlineIcon>
-          ) : (
-            <AdjustIcon className="icon-todo-progress"></AdjustIcon>
-          )}
-          <div className="title-block">
-            <p className="list-title">{props.task.title}</p>
-            <p className="secundary-text">
-              #{props.task.id} Created on {props.task.date}
-            </p>
-          </div>
-          <button className="basket" onClick={handleDelete}>
-            🗑️
-          </button>
-        </li>
-      ) : (
-        <li className={`list-element display-none`}>
-          {props.task.done ? (
-            <CheckCircleOutlineIcon className="icon-done"></CheckCircleOutlineIcon>
-          ) : (
-            <AdjustIcon className="icon-todo-progress"></AdjustIcon>
-          )}
-          <div className="title-block">
-            <p className="list-title">{props.task.title}</p>
-            <p className="secundary-text">
-              #{props.task.id} Created on {props.task.date}
-            </p>
-          </div>
-          <button className="basket" onClick={handleDelete}>
-            🗑️
-          </button>
-        </li>
-      )}
+        <li className={`list-element ${isHidden}`}>
+        {props.task.done ? (
+          <CheckCircleOutlineIcon className="icon-done"></CheckCircleOutlineIcon>
+        ) : (
+          <AdjustIcon className="icon-todo-progress"></AdjustIcon>
+        )}
+        <div className="title-block">
+          <p className="list-title">{props.task.title}</p>
+          <p className="secundary-text">
+            #{props.task.id} Created on {props.task.date}
+          </p>
+        </div>
+        <button className="basket" onClick={handleDelete}>
+          🗑️
+        </button>
+      </li>
     </React.Fragment>
   );
 }
